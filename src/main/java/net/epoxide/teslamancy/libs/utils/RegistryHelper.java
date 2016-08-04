d3a39df65d340e16b3f575d87abdc591341c3c60
Tyler Hancock <darklime@live.ca>
2016-08-04 15:19:14 -0600

    Added some docs for the registry stuff.

--- src/main/java/net/epoxide/teslamancy/libs/utils/RegistryHelper.java
+++ src/main/java/net/epoxide/teslamancy/libs/utils/RegistryHelper.java
@@ -12,11 +12,26 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class RegistryHelper {
     
+    /**
+     * Registers a block with a default item block.
+     * 
+     * @param block The block to register.
+     * @param blockName The name for the block. Should use snake case.
+     * @return The block that was registered.
+     */
     public static Block registerBlock (Block block, String blockName) {
         
         return registerBlock(block, blockName, new ItemBlock(block));
     }
     
+    /**
+     * Registers a block along side a custom item block.
+     * 
+     * @param block The block to register.
+     * @param blockName The name for the block. Should use snake case.
+     * @param itemBlock The item to use for the block.
+     * @return The block that was registered.
+     */
     public static Block registerBlock (Block block, String blockName, ItemBlock itemBlock) {
         
         block.setRegistryName(blockName);
@@ -28,6 +43,13 @@ public class RegistryHelper {
         return block;
     }
     
+    /**
+     * Registers an item.
+     * 
+     * @param item The item to register.
+     * @param itemName The name for the item. Should use snake case.
+     * @return The block that was registered.
+     */
     public static Item registerItem (Item item, String itemName) {
         
         item.setRegistryName(itemName);
@@ -38,53 +60,98 @@ public class RegistryHelper {
         return item;
     }
     
+    /**
+     * Registers an inventory model for a block, using a series of strings which represent
+     * model names in order of meta.
+     * 
+     * @param block The block to register models for.
+     * @param variants Array of variant names.
+     */
     public static void registerBlockInvModel (Block block, String[] variants) {
         
         registerItemInvModel(Item.getItemFromBlock(block), variants);
     }
     
+    /**
+     * Registers an inventory model for a block, using a prefix and a series of strings which
+     * represent model names in order of meta.
+     * 
+     * @param block The block to register models for.
+     * @param prefix The prefix to use on all models.
+     * @param variants Array of variant names.
+     */
     public static void registerBlockInvModel (Block block, String prefix, String[] variants) {
         
         registerItemInvModel(Item.getItemFromBlock(block), prefix, variants);
     }
     
+    /**
+     * Registers an inventory model for a basic block.
+     * 
+     * @param block The block to register models for.
+     */
     public static void registerBlockInvModel (Block block) {
         
-        registerItemInvModel(Item.getItemFromBlock(block), 0);
+        registerItemInvModel(Item.getItemFromBlock(block), 0, block.getRegistryName().toString());
     }
     
-    public static void registerBlockInvModel (Block block, int meta) {
-        
-        registerItemInvModel(Item.getItemFromBlock(block), meta);
-    }
-    
+    /**
+     * Registers an inventory model for an item.
+     * 
+     * @param item The item to register a model for.
+     * @param meta The meta for the model.
+     * @param model The model name.
+     */
     public static void registerItemInvModel (Item item, int meta, String model) {
         
         setCustomModelWrapper(item, meta, new ModelResourceLocation(model, "inventory"));
     }
     
+    /**
+     * Registers an inventory model for an item with multiple meta states.
+     * 
+     * @param item The item to register a model for.
+     * @param prefix A prefix for all the variant states.
+     * @param variants Array of variants in order of meta value.
+     */
     public static void registerItemInvModel (Item item, String prefix, String[] variants) {
         
         for (int meta = 0; meta < variants.length; meta++)
             setCustomModelWrapper(item, meta, new ModelResourceLocation(Constants.MODID + ":" + prefix + "_" + variants[meta], "inventory"));
     }
     
+    /**
+     * Registers an inventory model for an item with multiple meta states.
+     * 
+     * @param item The item to register a model for.
+     * @param variants Array of variants in order of meta value.
+     */
     public static void registerItemInvModel (Item item, String[] variants) {
         
         for (int meta = 0; meta < variants.length; meta++)
             setCustomModelWrapper(item, meta, new ModelResourceLocation(Constants.MODID + ":" + variants[meta], "inventory"));
     }
     
+    /**
+     * Register a basic inventory model for an item.
+     * 
+     * @param item The item to register a model for.
+     */
     public static void registerItemInvModel (Item item) {
         
         setCustomModelWrapper(item, 0, new ModelResourceLocation(item.getRegistryName().toString(), "inventory"));
     }
     
-    public static void registerItemInvModel (Item item, int meta) {
-        
-        setCustomModelWrapper(item, meta, new ModelResourceLocation(item.getRegistryName().toString(), "inventory"));
-    }
-    
+    /**
+     * A wrapper method for
+     * {@link ModelLoader#setCustomModelResourceLocation(Item, int, ModelResourceLocation)}
+     * that can be called server side. Doing this until I have time to work out a saner
+     * registry method.
+     * 
+     * @param item The item to register a model for.
+     * @param meta The meta to register a model at.
+     * @param model The model to register.
+     */
     private static void setCustomModelWrapper (Item item, int meta, ModelResourceLocation model) {
         
         if (FMLCommonHandler.instance().getSide().isClient())
