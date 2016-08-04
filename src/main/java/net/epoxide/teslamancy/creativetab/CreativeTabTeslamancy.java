d201c15d86ddc2ea0c2fa70c6e4c81526e9ba59e
Tyler Hancock <darklime@live.ca>
2016-08-04 15:01:03 -0600

    Made a seperate creative tab class.

---
+++ src/main/java/net/epoxide/teslamancy/creativetab/CreativeTabTeslamancy.java
@@ -0,0 +1,21 @@
+package net.epoxide.teslamancy.creativetab;
+
+import net.epoxide.teslamancy.handler.ContentHandler;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class CreativeTabTeslamancy extends CreativeTabs {
+
+    public static final CreativeTabs INSTANCE = new CreativeTabTeslamancy();
+    
+    public CreativeTabTeslamancy() {
+        
+        super("teslamancy");
+    }
+
+    @Override
+    public Item getTabIconItem () {
+        
+        return ContentHandler.itemWand;
+    }
+}
