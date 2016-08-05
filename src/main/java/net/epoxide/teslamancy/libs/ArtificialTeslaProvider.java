ee434dd1c89a81d95cce5aaddb6be74def77f91f
Tyler Hancock <darklime@live.ca>
2016-08-04 20:51:59 -0600

    Attempted to add an artificial tesla capability.

---
+++ src/main/java/net/epoxide/teslamancy/libs/ArtificialTeslaProvider.java
@@ -0,0 +1,55 @@
+package net.epoxide.teslamancy.libs;
+
+import net.darkhax.tesla.api.implementation.BaseTeslaContainer;
+import net.darkhax.tesla.capability.TeslaCapabilities;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.INBTSerializable;
+
+public class ArtificialTeslaProvider implements INBTSerializable<NBTTagCompound>, ICapabilityProvider {
+    
+    private final ArtificialTeslaContainer container;
+    
+    public ArtificialTeslaProvider() {
+        
+        this.container = new ArtificialTeslaContainer();
+    }
+    
+    @Override
+    public boolean hasCapability (Capability<?> capability, EnumFacing facing) {
+        
+        return capability == TeslaCapabilities.CAPABILITY_CONSUMER || capability == TeslaCapabilities.CAPABILITY_PRODUCER || capability == TeslaCapabilities.CAPABILITY_HOLDER;
+    }
+    
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T getCapability (Capability<T> capability, EnumFacing facing) {
+        
+        if (capability == TeslaCapabilities.CAPABILITY_CONSUMER || capability == TeslaCapabilities.CAPABILITY_PRODUCER || capability == TeslaCapabilities.CAPABILITY_HOLDER)
+            return (T) this.container;
+            
+        return null;
+    }
+    
+    @Override
+    public NBTTagCompound serializeNBT () {
+        
+        return this.container.serializeNBT();
+    }
+    
+    @Override
+    public void deserializeNBT (NBTTagCompound nbt) {
+        
+        this.container.deserializeNBT(nbt);
+    }
+    
+    public class ArtificialTeslaContainer extends BaseTeslaContainer {
+        
+        public ArtificialTeslaContainer() {
+            
+            super(500, 5, 5);
+        }
+    }
+}
---
+++ src/main/java/net/epoxide/teslamancy/libs/utils/CapabilityHelper.java
@@ -0,0 +1,96 @@
+package net.epoxide.teslamancy.libs.utils;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+import net.epoxide.teslamancy.libs.Constants;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.capabilities.CapabilityDispatcher;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.INBTSerializable;
+
+public class CapabilityHelper {
+    
+    private static boolean initialized = false;
+    private static Field capabilitiesField;
+    private static Field capsField;
+    private static Field writersField;
+    private static Field namesField;
+    
+    public static void addProvider (ItemStack stack, String key, ICapabilityProvider provider) {
+        
+        init();
+        
+        try {
+            
+            CapabilityDispatcher capabilities = (CapabilityDispatcher) capabilitiesField.get(stack);
+            
+            if (capabilities == null)
+                capabilities = new CapabilityDispatcher(new HashMap<ResourceLocation, ICapabilityProvider>());
+                
+            final ICapabilityProvider[] caps = (ICapabilityProvider[]) capsField.get(capabilities);
+            final String[] names = (String[]) namesField.get(capabilities);
+            
+            for (String name : names)
+                if (name.equals(key)) {
+                    
+                    Constants.LOGGER.warn("Attempted to add duplicate providers!");
+                    return;
+                }
+                
+            final ICapabilityProvider[] expandedCaps = new ICapabilityProvider[caps.length + 1];
+            expandedCaps[expandedCaps.length - 1] = provider;
+            System.arraycopy(caps, 0, expandedCaps, 0, caps.length);
+            capsField.set(capabilities, expandedCaps);
+            
+            if (provider instanceof INBTSerializable) {
+                
+                final INBTSerializable<NBTBase>[] writers = (INBTSerializable<NBTBase>[]) writersField.get(capabilities);
+                final INBTSerializable[] expandedWriters = new INBTSerializable[writers.length + 1];
+                expandedWriters[expandedWriters.length - 1] = (INBTSerializable) provider;
+                System.arraycopy(writers, 0, expandedWriters, 0, writers.length);
+                writersField.set(capabilities, expandedWriters);
+                
+                final String[] expandedNames = new String[names.length + 1];
+                System.arraycopy(names, 0, expandedNames, 0, names.length);
+                expandedNames[expandedNames.length - 1] = key;
+                namesField.set(capabilities, expandedNames);
+            }
+            
+            capabilitiesField.set(stack, capabilities);
+        }
+        
+        catch (IllegalArgumentException | IllegalAccessException exception) {
+            
+            Constants.LOGGER.warn(exception);
+        }
+    }
+    
+    private static void init () {
+        
+        if (!initialized) {
+            
+            try {
+                
+                capabilitiesField = ItemStack.class.getDeclaredField("capabilities");
+                capabilitiesField.setAccessible(true);
+                
+                capsField = CapabilityDispatcher.class.getDeclaredField("caps");
+                capsField.setAccessible(true);
+                
+                writersField = CapabilityDispatcher.class.getDeclaredField("writers");
+                writersField.setAccessible(true);
+                
+                namesField = CapabilityDispatcher.class.getDeclaredField("names");
+                namesField.setAccessible(true);
+            }
+            
+            catch (NoSuchFieldException | SecurityException exception) {
+                
+                Constants.LOGGER.warn(exception);
+            }
+        }
+    }
+}
