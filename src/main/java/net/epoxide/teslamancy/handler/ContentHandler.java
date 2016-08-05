883dcecd7a20cde5222874f83691a41beeee5f70
Tyler Hancock <darklime@live.ca>
2016-08-04 21:48:51 -0600

    Added a loadstone item

--- src/main/java/net/epoxide/teslamancy/handler/ContentHandler.java
+++ src/main/java/net/epoxide/teslamancy/handler/ContentHandler.java
@@ -1,36 +1,39 @@
 package net.epoxide.teslamancy.handler;
 
+import static net.epoxide.teslamancy.libs.utils.RegistryHelper.registerBlock;
+import static net.epoxide.teslamancy.libs.utils.RegistryHelper.registerItem;
+import static net.epoxide.teslamancy.libs.utils.RegistryHelper.registerItemInvModel;
+
 import net.epoxide.teslamancy.block.BlockInductionPad;
 import net.epoxide.teslamancy.block.BlockPlasmaGenerator;
 import net.epoxide.teslamancy.block.BlockPrimalPlasma;
 import net.epoxide.teslamancy.block.tileentity.TileEntityInductionPad;
 import net.epoxide.teslamancy.block.tileentity.TileEntityPlasmaGenerator;
 import net.epoxide.teslamancy.fluid.FluidPrimalPlasma;
+import net.epoxide.teslamancy.item.ItemLoadstone;
 import net.epoxide.teslamancy.item.ItemMaterial;
 import net.epoxide.teslamancy.item.ItemPowerCell;
 import net.epoxide.teslamancy.item.ItemWand;
-
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-
-import static net.epoxide.teslamancy.libs.utils.RegistryHelper.*;
 
 public class ContentHandler {
     
     public static Block blockPlasmaGenerator;
     public static Block blockInductionPad;
     public static Block blockPrimalPlasma;
-
+    
     public static Item itemMaterial;
     public static Item itemWand;
     public static Item itemPowercell;
-
+    public static Item itemLoadstone;
+    
     public static void init () {
-
+        
         FluidRegistry.registerFluid(FluidPrimalPlasma.INSTANCE);
-
+        
         initBlock();
         initItem();
     }
@@ -42,7 +45,7 @@ public class ContentHandler {
         
         blockInductionPad = registerBlock(new BlockInductionPad(), "induction_pad");
         GameRegistry.registerTileEntity(TileEntityInductionPad.class, "induction_pad");
-
+        
         blockPrimalPlasma = registerBlock(new BlockPrimalPlasma(), "primal_plasma");
     }
     
@@ -52,8 +55,11 @@ public class ContentHandler {
         registerItemInvModel(itemMaterial, "material", ItemMaterial.VARIANTS);
         
         itemWand = registerItem(new ItemWand(), "wand");
-
+        
         itemPowercell = registerItem(new ItemPowerCell(), "powercell");
         registerItemInvModel(itemPowercell);
+        
+        itemLoadstone = registerItem(new ItemLoadstone(), "loadstone");
+        registerItemInvModel(itemLoadstone);
     }
 }
---
+++ src/main/java/net/epoxide/teslamancy/item/ItemLoadstone.java
@@ -0,0 +1,74 @@
+package net.epoxide.teslamancy.item;
+
+import java.util.List;
+
+import net.minecraft.client.resources.I18n;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemLoadstone extends Item {
+    
+    @Override
+    public void onUpdate (ItemStack stack, World world, Entity entity, int slot, boolean isHeld) {
+        
+        if (isEnabled(stack) && !entity.isSneaking()) {
+            
+            final int range = 3;
+            final float factor = 0.025f;
+            
+            final double x = entity.posX;
+            final double y = entity.posY + 1.5;
+            final double z = entity.posZ;
+            
+            final List<EntityItem> items = world.getEntitiesWithinAABB(EntityItem.class, new AxisAlignedBB(x - range, y - range, z - range, x + range, y + range, z + range));
+            
+            for (final EntityItem item : items)
+                item.addVelocity((x - item.posX) * factor, (y - item.posY) * factor, (z - item.posZ) * factor);
+        }
+    }
+    
+    @Override
+    public ActionResult<ItemStack> onItemRightClick (ItemStack stack, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+        
+        if (!stack.hasTagCompound())
+            stack.setTagCompound(new NBTTagCompound());
+            
+        stack.getTagCompound().setBoolean("Enabled", !isEnabled(stack));
+        return new ActionResult(EnumActionResult.PASS, stack);
+    }
+    
+    public static boolean isEnabled (ItemStack stack) {
+        
+        return stack != null && stack.hasTagCompound() && stack.getTagCompound().getBoolean("Enabled");
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation (ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced) {
+        
+        tooltip.add(I18n.format("tooltip.teslamancy.loadstone." + isEnabled(stack)));
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubItems (Item item, CreativeTabs tab, List<ItemStack> subItems) {
+        
+        final ItemStack stack = new ItemStack(item);
+        stack.setTagCompound(new NBTTagCompound());
+        stack.getTagCompound().setBoolean("Enabled", true);
+        subItems.add(stack);
+        subItems.add(new ItemStack(item));
+    }
+}
