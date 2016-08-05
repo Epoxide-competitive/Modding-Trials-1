5d9b6634c620c1f2522a3b04d1998d882efdc896
Tyler Hancock <darklime@live.ca>
2016-08-04 20:52:25 -0600

    Set up a logger.

--- src/main/java/net/epoxide/teslamancy/libs/Constants.java
+++ src/main/java/net/epoxide/teslamancy/libs/Constants.java
@@ -1,7 +1,12 @@
 package net.epoxide.teslamancy.libs;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Constants {
+    
     public static final String MODID = "teslamancy";
     public static final String MOD_NAME = "Teslamancy";
     public static final String VERSION = "0.0.0.1";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 }
