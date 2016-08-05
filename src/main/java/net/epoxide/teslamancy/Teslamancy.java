63fded2c5ab1a7bbfb53303a71fa9036aa083089
Tyler Hancock <darklime@live.ca>
2016-08-04 20:52:12 -0600

    Added events

--- src/main/java/net/epoxide/teslamancy/Teslamancy.java
+++ src/main/java/net/epoxide/teslamancy/Teslamancy.java
@@ -1,8 +1,10 @@
 package net.epoxide.teslamancy;
 
 import net.epoxide.teslamancy.handler.ContentHandler;
+import net.epoxide.teslamancy.handler.ForgeEventHandler;
 import net.epoxide.teslamancy.handler.GuiHandler;
 import net.epoxide.teslamancy.libs.Constants;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -19,6 +21,7 @@ public class Teslamancy {
         
         ContentHandler.init();
         NetworkRegistry.INSTANCE.registerGuiHandler(INSTANCE, new GuiHandler());
+        MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
     }
     
     @Mod.EventHandler
---
+++ src/main/java/net/epoxide/teslamancy/handler/ForgeEventHandler.java
@@ -0,0 +1,5 @@
+package net.epoxide.teslamancy.handler;
+
+public class ForgeEventHandler {
+
+}
